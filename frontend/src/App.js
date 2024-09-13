4f3b11a0064facbb3c5aac30eb940fbccd69cfb1
mfab9 <rhysget0@gmail.com>
2024-09-13 11:29:25 +0530

    fix error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,14 @@ import api from './api/axiosconfig';
 import { useState, useEffect } from 'react';
 import Layout from './components/Layout';
 import {Routes,Route} from  "react-router-dom";
-import Home from './components/Home/Home';
+import Home from './components/home/Home';
 function App() {
-  const [movie, setMovies] = useState();
+  const [movies, setMovies] = useState();
   const getMovies = async () => {
     try {
       const response = await api.get("/api/v1/movies");
       console.log(response.data);
-      
+
       setMovies(response.data);
     } catch (err) {
       console.log(err);
@@ -24,7 +24,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Layout/>}>
-        <Route path="/" element={<Home/>}> </Route>
+        <Route path="/" element={<Home movies={movies}/>}> </Route>
         </Route>
       </Routes>
     </div>
--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Hero from '../herrro/Hero'
+
 
 const Home = ({movies}) => {
   return (
-    <div>Welcome</div>
+    <Hero movies={movies}/>
   )
 }
 
