7fae6ce6a6946860f60b31e800e491865123c31c
mfab9 <rhysget0@gmail.com>
2024-09-15 21:59:26 +0530

    css setup

--- frontend/src/components/herrro/Hero.js
+++ frontend/src/components/herrro/Hero.js
@@ -10,8 +10,8 @@ const Hero = ({movies}) => {
                 movies.map((movie)=>{
                     return(
                         <Paper>
-                            <div className='movie-card-container'>
-                                <div className='movie-card'>
+                            <div className='movie-card-container'> 
+                                <div className='movie-card' style={{"-- img":`url(${movie.backdrops[0]})`}}>
                                     <div className='movie-detail'>
                                         <div className='movie-poster'>
                                             <img src={movie.poster} alt=''/>
