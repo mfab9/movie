e0e48a1f9773dbecac8efe02c49b551f5c5204be
ishan01k <ishan05kapoor@gmail.com>
2024-09-18 20:22:46 +0530

    hello world

--- backend/src/main/java/dev/javaproject/movies/MoviesApplication.java
+++ backend/src/main/java/dev/javaproject/movies/MoviesApplication.java
@@ -2,12 +2,21 @@ package dev.javaproject.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
+@RestController
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
+	@GetMapping("/")
+	public String apiRoot() {
+		return "Hello, World!";
+	}	
 
 }
