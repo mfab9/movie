1101693be3d51aaf21d964c4b635726ca317f140
ishan01k <ishan05kapoor@gmail.com>
2024-09-24 18:07:30 +0530

    Update MoviesApplication.java

--- backend/src/main/java/dev/javaproject/movies/MoviesApplication.java
+++ backend/src/main/java/dev/javaproject/movies/MoviesApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 
 
 @SpringBootApplication
