f147ceb8f84541d24c94a88d97bbae2a5c5f04b3
mfab9 <rhysget0@gmail.com>
2024-10-01 23:15:11 +0530

    controller api/v1/movies ,repoMongo, Services

--- backend/src/main/java/dev/javaproject/movies/Movie.java
+++ backend/src/main/java/dev/javaproject/movies/Movie.java
@@ -10,7 +10,7 @@ import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
-@Document(collection = "moviec")
+@Document(collection = "movies")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
