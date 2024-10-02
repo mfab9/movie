0dc5c1103f78c8eb6861848fb1c1737b5d80ec82
ishan01k <ishan05kapoor@gmail.com>
2024-10-02 12:29:00 +0530

    116time

--- backend/src/main/java/dev/javaproject/movies/MovieController.java
+++ backend/src/main/java/dev/javaproject/movies/MovieController.java
@@ -1,13 +1,19 @@
 package dev.javaproject.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -18,4 +24,9 @@ public class MovieController  {
     public ResponseEntity<List<Movie> > allMovies() {
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>>  getSingleMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<Optional<Movie>>(movieService.SingleMovie(imdbId), HttpStatus.OK);
+    }   
 }
