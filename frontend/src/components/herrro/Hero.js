7058d6bf8043932298534b7a771e854a793f751a
mfab9 <rhysget0@gmail.com>
2024-10-02 13:16:17 +0530

    map bug in hero js

--- frontend/src/components/herrro/Hero.js
+++ frontend/src/components/herrro/Hero.js
@@ -7,14 +7,14 @@ const Hero = ({movies}) => {
     <div>
         <Carousel>
             {
-                movies.map((movie)=>{
+                movies?.map((movie)=>{
                     return(
-                        <Paper>
+                        <Paper key={movie.id}>
                             <div className='movie-card-container'> 
-                                <div className='movie-card' style={{"-- img":`url(${movie.backdrops[0]})`}}>
+                                <div className='movie-card' style={{"--img":`url(${movie.backdrops[0]})`}}>
                                     <div className='movie-detail'>
                                         <div className='movie-poster'>
-                                            <img src={movie.poster} alt=''/>
+                                            <img src={movie.poster} alt='movie title'/>
                                         </div>
                                         <div className='movie-title'>
                                             <h4>{movie.title}</h4>
