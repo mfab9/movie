72c81b4a52802b39e9206beb45b9cdbc1344a296
mfab9 <rhysget0@gmail.com>
2024-10-02 13:02:02 +0530

    adding backend to frontend

--- backend/src/main/java/dev/javaproject/movies/MovieController.java
+++ backend/src/main/java/dev/javaproject/movies/MovieController.java
@@ -4,17 +4,12 @@ package dev.javaproject.movies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.web.bind.annotation.RequestParam;
-
-
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController  {
