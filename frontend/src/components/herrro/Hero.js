a70a3ddfecc8a43048e4d456a685512f40b4296e
mfab9 <rhysget0@gmail.com>
2024-10-11 17:08:53 +0530

    frontend code continue"

--- frontend/src/components/herrro/Hero.js
+++ frontend/src/components/herrro/Hero.js
@@ -2,6 +2,10 @@ import React from 'react'
 import './Hero.css';
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import {Link, useNavigate} from "react-router-dom";
+import { Button } from 'bootstrap';
 const Hero = ({movies}) => {
   return (
     <div>
@@ -18,6 +22,17 @@ const Hero = ({movies}) => {
                                         </div>
                                         <div className='movie-title'>
                                             <h4>{movie.title}</h4>
+                                        </div>
+                                        <div className='movie-buttons-container'>
+                                            <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
+                                            <div className='play-button-icon-container'>
+                                                <FontAwesomeIcon className='play-button-icon' icon={faCirclePlay}/>
+                                            </div>
+                                            </Link>
+
+                                            <div className="movie-review-button-container">
+                                                <Button variant ="info" onClick={() => reviews(movie.imdbId)} >Reviews</Button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
